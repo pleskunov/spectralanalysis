47c744198eb396e2e4f52b290e5709d06c1c419b
Paul Pleskunov <pp@positroncloud.com>
2025-01-27 00:12:09 -0500

    updated build and added simple tests for Oscillators

--- src/spectralanalysis.cpp
+++ src/spectralanalysis.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
-#include "oscillators/genosc.hpp"
+#include "oscillators/drude.hpp"
 
-int main(int argc, char** argv) {
-  std::cout << "Hello World!";
+int main() {
+    DrudeOscillatorRT drude("Drude Oscillator", 1.0, 2.0);
+    Oscillator genericOscillator("Generic Oscillator", GENERIC);
+
+    std::vector<double> energies = {1.0, 2.0, 3.0};
+
+    std::vector<std::complex<double>> epsilon = drude.compute_permittivity(energies);
+
+    for (const auto& eps : epsilon) {
+      std::cout << eps << std::endl;
+    }
+
+    std::cout << "Drude type: " << drude.getType() << std::endl;
+    std::cout << "Generic type: " << genericOscillator.getType() << std::endl;
+
+    return 0;
 }
