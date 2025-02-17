52190050979bbebf60710b0844c93d4bac1817ac
Paul Pleskunov <pp@positroncloud.com>
2025-02-16 22:07:09 -0500

    Lorentz: namespace, cleanup and test

--- src/oscillators/lorentz.cpp
+++ src/oscillators/lorentz.cpp
@@ -1,16 +1,20 @@
 #include "lorentz.hpp"
 #include "constants.hpp"
 
-std::complex<double> LorentzOscillator::compute_epsilon_at(double photonEnergy) const {
-  std::complex<double> numerator = parameters.Amp * parameters.Br * parameters.En;
-  std::complex<double> denominator = (parameters.En * parameters.En) - (photonEnergy * photonEnergy) - parameters.Br * photonEnergy * Constants::ImU;
+std::complex<double> Lorentz::OscillatorAmp::compute_epsilon_at(double photonEnergy) const {
+  double E2 = photonEnergy * photonEnergy;
+
+  std::complex<double> numerator = (parameters.Amp * parameters.Br * parameters.En) * Constants::ReU;
+  std::complex<double> denominator = (parameters.En * parameters.En) - E2 - parameters.Br * photonEnergy * Constants::ImU;
 
   return numerator / denominator;
 };
 
-std::complex<double> LorentzImAmpOscillator::compute_epsilon_at(double photonEnergy) const {
+std::complex<double> Lorentz::OscillatorImAmp::compute_epsilon_at(double photonEnergy) const {
+  double E2 = photonEnergy * photonEnergy;
+
   std::complex<double> numerator = (parameters.Amp + parameters.iAmp * Constants::ImU) * parameters.Br * parameters.En;
-  std::complex<double> denominator = (parameters.En * parameters.En) - (photonEnergy * photonEnergy) - parameters.Br * photonEnergy * Constants::ImU;
+  std::complex<double> denominator = (parameters.En * parameters.En) - E2 - parameters.Br * photonEnergy * Constants::ImU;
 
   return numerator / denominator;
 };
--- src/oscillators/lorentz.hpp
+++ src/oscillators/lorentz.hpp
@@ -4,36 +4,38 @@
 #include "genosc.hpp"
 #include "types.hpp"
 
-class Parameters {
-  public:
-    double Amp;
-    double iAmp;
-    double En;
-    double Br;
+namespace Lorentz {
+  class Parameters {
+    public:
+      double Amp;
+      double iAmp;
+      double En;
+      double Br;
 
-  Parameters(double Amp = 0.0, double iAmp = 0.0, double En = 0.0, double Br = 0.0)
-    : Amp(Amp),
-      iAmp(iAmp),
-      En(En),
-      Br(Br) {}
-};
+    Parameters(double Amp, double iAmp, double En, double Br)
+      : Amp(Amp),
+        iAmp(iAmp),
+        En(En),
+        Br(Br) {}
+  };
 
-class LorentzOscillator : public Oscillator {
-  public:
-    Parameters parameters;
+  class OscillatorAmp : public Oscillator {
+    public:
+      Parameters parameters;
 
-    LorentzOscillator(const std::string& name, double Amp, double En, double Br) : Oscillator(name, LORENTZ), parameters(Amp, 0.0, En, Br) {}
+      OscillatorAmp(const std::string& name, double Amp, double En, double Br) : Oscillator(name, LORENTZ), parameters(Amp, 0.0, En, Br) {}
 
-    std::complex<double> compute_epsilon_at(double photonEnergy) const override;
-};
+      std::complex<double> compute_epsilon_at(double photonEnergy) const override;
+  };
 
-class LorentzImAmpOscillator : public Oscillator {
-  public:
-    Parameters parameters;
+  class OscillatorImAmp : public Oscillator {
+    public:
+      Parameters parameters;
 
-    LorentzImAmpOscillator(const std::string& name, double Amp, double iAmp, double En, double Br) : Oscillator(name, LORENTZ_IMAMP), parameters(Amp, iAmp, En, Br) {}
+      OscillatorImAmp(const std::string& name, double Amp, double iAmp, double En, double Br) : Oscillator(name, LORENTZ_IMAMP), parameters(Amp, iAmp, En, Br) {}
 
-    std::complex<double> compute_epsilon_at(double photonEnergy) const override;
-};
+      std::complex<double> compute_epsilon_at(double photonEnergy) const override;
+  };
+} // namespace Lorentz
 
 #endif // !LORENTZ_HPP
