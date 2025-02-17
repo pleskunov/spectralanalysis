90284ce3117a3d33c54eb098d11fb764e7b43b6e
Paul Pleskunov <pp@positroncloud.com>
2025-02-16 23:10:17 -0500

    Harmonic: namespace and cleanup

--- src/oscillators/harmonic.cpp
+++ src/oscillators/harmonic.cpp
@@ -3,21 +3,20 @@
 
 #include <complex>
 
-std::complex<double> HarmonicOscillator::compute_epsilon_at(double photonEnergy) const {
-  static double prefactor = (parameters.Amp * parameters.Br) / 2.0;
+std::complex<double> Harmonic::OscillatorAmp::compute_epsilon_at(double photonEnergy) const {
+  double prefactor = (parameters.Amp * parameters.Br) / 2.0;
 
-  static std::complex<double> t1 = 1.0 / (parameters.En - photonEnergy - 0.5 * parameters.Br * Constants::ImU);
-  static std::complex<double> t2 = 1.0 / (parameters.En + photonEnergy - 0.5 * parameters.Br * Constants::ImU);
+  std::complex<double> t1 = 1.0 / (parameters.En - photonEnergy - 0.5 * parameters.Br * Constants::ImU);
+  std::complex<double> t2 = 1.0 / (parameters.En + photonEnergy - 0.5 * parameters.Br * Constants::ImU);
 
-  return (prefactor * Constants::ReU) * (t1 + t2);
+  return prefactor * (t1 + t2);
 }
 
+std::complex<double> Harmonic::OscillatorImAmp::compute_epsilon_at(double photonEnergy) const {
+  std::complex<double> prefactor = ((parameters.Amp + parameters.iAmp * Constants::ImU) * parameters.Br) / 2.0;
 
-std::complex<double> HarmonicImAmpOscillator::compute_epsilon_at(double photonEnergy) const {
-  static std::complex<double> prefactor = ((parameters.Amp + parameters.iAmp * Constants::ImU) * parameters.Br) / 2.0;
-
-  static std::complex<double> t1 = 1.0 / (parameters.En - photonEnergy - 0.5 * parameters.Br * Constants::ImU);
-  static std::complex<double> t2 = 1.0 / (parameters.En + photonEnergy - 0.5 * parameters.Br * Constants::ImU);
+  std::complex<double> t1 = 1.0 / (parameters.En - photonEnergy - 0.5 * parameters.Br * Constants::ImU);
+  std::complex<double> t2 = 1.0 / (parameters.En + photonEnergy - 0.5 * parameters.Br * Constants::ImU);
 
   return prefactor * (t1 + t2);
 }
--- src/oscillators/harmonic.hpp
+++ src/oscillators/harmonic.hpp
@@ -4,36 +4,38 @@
 #include "genosc.hpp"
 #include "types.hpp"
 
-class Parameters {
-  public:
-    double Amp;
-    double iAmp;
-    double En;
-    double Br;
+namespace Harmonic {
+  class Parameters {
+    public:
+      double Amp;
+      double iAmp;
+      double En;
+      double Br;
 
-  Parameters(double Amp = 0.0, double iAmp = 0.0, double En = 0.0, double Br = 0.0)
-    : Amp(Amp),
-      iAmp(iAmp),
-      En(En),
-      Br(Br) {}
-};
+    Parameters(double Amp = 0.0, double iAmp = 0.0, double En = 0.0, double Br = 0.0)
+      : Amp(Amp),
+        iAmp(iAmp),
+        En(En),
+        Br(Br) {}
+  };
 
-class HarmonicOscillator : public Oscillator {
-  public:
-    Parameters parameters;
+  class OscillatorAmp : public Oscillator {
+    public:
+      Parameters parameters;
 
-    HarmonicOscillator(const std::string& name, double Amp, double En, double Br) : Oscillator(name, HARMONIC), parameters(Amp, 0.0, En, Br) {}
+      OscillatorAmp(const std::string& name, double Amp, double En, double Br) : Oscillator(name, HARMONIC), parameters(Amp, 0.0, En, Br) {}
 
-    std::complex<double> compute_epsilon_at(double photonEnergy) const override;
-};
+      std::complex<double> compute_epsilon_at(double photonEnergy) const override;
+  };
 
-class HarmonicImAmpOscillator : public Oscillator {
-  public:
-    Parameters parameters;
+  class OscillatorImAmp : public Oscillator {
+    public:
+      Parameters parameters;
 
-    HarmonicImAmpOscillator(const std::string& name, double Amp, double iAmp, double En, double Br) : Oscillator(name, HARMONIC_IMAMP), parameters(Amp, iAmp, En, Br) {}
+      OscillatorImAmp(const std::string& name, double Amp, double iAmp, double En, double Br) : Oscillator(name, HARMONIC_IMAMP), parameters(Amp, iAmp, En, Br) {}
 
-    std::complex<double> compute_epsilon_at(double photonEnergy) const override;
-};
+      std::complex<double> compute_epsilon_at(double photonEnergy) const override;
+  };
+} // namespace Harmonic
 
 #endif // !HARMONIC_HPP
